d60205dd3132b31039a7894573e9616a0ed679cd
akshatshandilya6869 <akshatshandilya203@gmail.com>
2025-03-24 00:10:07 +0530

    Databse Connected

--- src/main/java/com/Address/AddressBookApp/controller/AddressController.java
+++ src/main/java/com/Address/AddressBookApp/controller/AddressController.java
@@ -1,10 +1,11 @@
-package com.example.AddressBookApp.controller;
+package com.Address.AddressBookApp.controller;
 
 import com.Address.AddressBookApp.dto.EmployeeDTO;
 import com.Address.AddressBookApp.dto.ResponseDTO;
 import com.Address.AddressBookApp.interfaces.EmployeeInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/address")
@@ -70,5 +71,42 @@ public class AddressController {
     @DeleteMapping("/res2/delete/{id}")
     public ResponseDTO delete2(@PathVariable Long id){
         return Iemployee.res("API triggered", "Success");
+    }
+
+
+
+
+
+
+    //UC3 (Handling REST API using service layer with database)
+
+    @GetMapping("/get/{id}")
+    public EmployeeDTO get3(@PathVariable Long id){
+        return Iemployee.get(id);
+    }
+
+    @PostMapping("/create")
+    public EmployeeDTO create3(@RequestBody EmployeeDTO user){
+        return Iemployee.create(user);
+    }
+
+    @GetMapping("/getAll")
+    public List<EmployeeDTO> getAll3(){
+        return Iemployee.getAll();
+    }
+
+    @PutMapping("/edit/{id}")
+    public EmployeeDTO edit3(@RequestBody EmployeeDTO user, @PathVariable Long id){
+        return Iemployee.edit(user, id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String delete3(@PathVariable Long id){
+        return Iemployee.delete(id);
+    }
+
+    @GetMapping("/clear")
+    public String clear(){
+        return Iemployee.clear();
     }
 }
